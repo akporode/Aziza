ffd124331b9adad6d39b50a0db681ca722cfb10b
lifeisbig <akpo20@gmail.com>
2017-01-22 22:00:10 -0700

    TimeSheet App Re-arrange folders

---
+++ github-repository/react-frontend/components/TimeSheetWeekContainer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import TimeSheetWeekOption from './TimeSheetWeekOption';
+
+export default class TimeSheetWeekContainer extends React.Component{
+	
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+
+	}
+	handleChange(event) {
+   
+
+    //when a different week option is changed notify "TimeSheetContainer", who is parent component of TimeSheetWeekContainer
+    this.props.onUserSelect(event.target.value);
+
+
+    console.log("Logging handleChange() " + event.target.value);
+}
+ /*
+	* lifecycle component is called after component is mounted;
+	*/
+	componentDidMount() { 
+		console.log("entering TimeSheetWeekContainer componentDidMount" );
+		
+		
+	}
+	
+
+render(){
+	this.props.weeks.forEach(function(item){
+		console.log( "each week item " + item);
+	});
+var unique = [...new Set(this.props.weeks.map(item => item))];
+
+var options = [];
+unique.forEach(function(item){
+	options.push(<TimeSheetWeekOption key={item} weekName={item} />);
+});
+
+return (
+	<div>
+	<div>
+	<h3>Weeks</h3>
+	</div>
+	<select value={this.props.filterByWeek} onChange={this.handleChange}>
+	{options}
+	</select>
+
+	</div>
+	);
+}	
+
+}
