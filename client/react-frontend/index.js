535494bfb171ec63945c753915f11c50f9ce4cdc
lifeisbig <akpo20@gmail.com>
2017-01-22 20:28:52 -0700

    TimeSheet App Intial Check In

---
+++ client/react-frontend/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TimeSheetApp from './TimeSheetApp';
+
+ReactDOM.render(
+   <TimeSheetApp />,
+   document.getElementById('app')
+);
