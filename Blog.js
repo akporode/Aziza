9a4d505e70fc9f78e3ee26f5db78eaed433e31f6
lifeisbig <akpo20@gmail.com>
2017-01-22 22:15:11 -0700

    TimeSheet App Remove Non-pertitent files

--- Blog.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Blog extends React.Component{
-	render(){
-	return (
-	 <div className="container">
-		<h1> Blogger of all things Boxing </h1></div>)
-	}
-}
--- NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-export default class NotFound extends React.Component{
-	
-	render(){
-		return ( <h1>404.. This page is not found!</h1>);
-	}
-}
