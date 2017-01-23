50408da0e3b847d18f9d6b695f041b826e4b9d98
lifeisbig <akpo20@gmail.com>
2017-01-22 22:23:33 -0700

    Remove duplicated directories

--- client/react-frontend/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import TimeSheetApp from './TimeSheetApp';
-
-ReactDOM.render(
-   <TimeSheetApp />,
-   document.getElementById('app')
-);
